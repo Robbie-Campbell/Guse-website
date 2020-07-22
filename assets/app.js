43d5e202bc7d0954537ecb392beea32641019347
LFCRab <56073739+LFCRab@users.noreply.github.com>
2020-07-22 20:28:39 +0100

    Added comments :pencil:

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,14 @@
+// Define the variables for the introduction
 const welcomeText = document.getElementById("intro");
 const logo = document.querySelector(".logo");
 const launchHomepage = document.querySelector(".launch-home")
 let introText = ["Welcome", "to","the", "future"];
 let index = 0;
 
+// Set an interval to connect with the animation timing
 let timer = setInterval(nextWord, 1000)
+
+// The main function to loop through the text array
 function nextWord(){
     welcomeText.textContent = introText[index];
     if (index < 4){
@@ -16,11 +20,13 @@ function nextWord(){
         launchHomepage.classList.add("showLogo");
     }
     else{
+        // Break the loop
         clearInterval(timer);
     }
     index++;
 }
 
+// Add a goose sound effect when the href is clicked
 launchHomepage.addEventListener("click", ()=>{
     let honk = new Audio("/assets/sounds/honk.mp3");
     honk.play();
